77927625d1360f700b6068f9b74e82d17ad06436
iumut <iumutcakici@gmail.com>
2022-03-16 00:59:14 +0300

    isPdf method added

--- src/scripts/validator.js
+++ src/scripts/validator.js
@@ -3,7 +3,11 @@ let validator = {
     isEmail(text){
         const regex = /^\w+@\w+[.]+\w+$/gm;
         return regex.test(text)
-}
+},
+    isPdf(text){
+        const regex =/^.+[.]pdf$/gmi;
+        return regex.test(text);
+    }
 
 };
 export default validator;
--- test/validator.js
+++ test/validator.js
@@ -2,6 +2,34 @@ import assert from 'assert';
 import validator from '../src/scripts/validator';
 describe('validator', () => {
 
+    describe('isPdf()', () =>{
+
+        it('should return false for nonPdf file names', function () {
+            assert.equal(validator.isPdf('deneme.txt'), false)
+            assert.equal(validator.isPdf('deneme.jpg'), false)
+            assert.equal(validator.isPdf('deneme.mobi'), false)
+            assert.equal(validator.isPdf('deneme.json'), false)
+            assert.equal(validator.isPdf('deneme.docx'), false)
+            assert.equal(validator.isPdf('deneme'), false)
+            assert.equal(validator.isPdf('onur.*pdf'), false)
+            assert.equal(validator.isPdf('o nur . pdf'), false)
+            assert.equal(validator.isPdf('o nur . pdf'), false)
+            assert.equal(validator.isPdf('onur.pdfx'), false)
+            assert.equal(validator.isPdf('abc.p d f'), false)
+        });
+
+        it('should return true for pdf file names', function () {
+            assert.equal(validator.isPdf('dergi.pdf'), true)
+            assert.equal(validator.isPdf('dergi(1).pdf'), true)
+            assert.equal(validator.isPdf('on ur.pdf'), true)
+            assert.equal(validator.isPdf('team_work.pdf'), true)
+            assert.equal(validator.isPdf('team 1234 @5.pdf'), true)
+            assert.equal(validator.isPdf('Monzo_bank_statement_2019_09_04-2021_08_22_6822.pdf'), true)
+            assert.equal(validator.isPdf('onur.PDF'), true)
+
+        });
+    })
+
     describe('#isEmail()', () => {
 
         it('should return false for invail email adresses', ()=> {
